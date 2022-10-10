e374819e22855c2a8d9efb71fa0854e1fa2f66de
kadenyi <treykadenyi@gmail.com>
2022-10-10 16:08:07 +0300

    Define item class properties

--- ruby/item.rb
+++ ruby/item.rb
@@ -1,3 +1,10 @@
 class Item
-  def init; end
+  attr_accessor :publish_date, :archived, :genre, :author, :source, :label
+  attr_reader :id
+
+  def initialize(publish_date, archived)
+    @id = Random.rand(1...1000)
+    @publish_date = publish_date
+    @archived = archived
+  end
 end
