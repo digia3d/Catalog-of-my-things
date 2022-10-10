b3efeb067860b7de42a4347916fa761fe1225cad
OkoyeCharles <okoyecharles509@gmail.com>
2022-10-10 16:45:30 +0100

    Refactor main.rb startup actions

--- ruby/app.rb
+++ ruby/app.rb
@@ -1,34 +1,5 @@
 class App
   def initialize
-    @running = true
-  end
-
-  def start
-    while @running
-      puts "
-       1. List all books
-       2. List all music albums
-       3. List all movies
-       4. List of games
-       5. List all genres
-       6. List all labels
-       7. List all authors
-       8. List all sources
-       9. Add a book
-      10. Add a music album
-      11. Add a movie
-      12. Add a game
-      13. Exit
-      "
-
-      option = gets.chomp.to_i
-      case option
-      when 13
-        @running = false
-        print 'Thanks for using the app!'
-      else
-        print "Invalid option... Choose one of the options above\n"
-      end
-    end
+    @name = 'John Doe'
   end
 end
--- ruby/main.rb
+++ ruby/main.rb
@@ -3,7 +3,43 @@ require_relative 'app'
 def main
   print "\nWelcome to catalog of my things\n"
   app = App.new
-  app.start
+  loop do
+    print_options
+    option = gets.chomp.to_i
+
+    handle_case(option, app)
+
+    break if option == 13
+  end
+end
+
+def handle_case(option, _app)
+  case option
+  when 1
+    print 'Coming Soon...'
+  when 13
+    print 'Thanks for using the app!'
+  else
+    print "Invalid option... Choose one of the options above\n"
+  end
+end
+
+def print_options
+  puts "
+   1. List all books
+   2. List all music albums
+   3. List all movies
+   4. List of games
+   5. List all genres
+   6. List all labels
+   7. List all authors
+   8. List all sources
+   9. Add a book
+  10. Add a music album
+  11. Add a movie
+  12. Add a game
+  13. Exit
+  "
 end
 
 main
