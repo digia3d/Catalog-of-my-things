517983efd1c8bdd06c81c09ab25fff3b290b0121
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-10-10 18:23:48 +0100

    Define properties for Movie Class

--- movie/movie.rb
+++ movie/movie.rb
@@ -1,2 +1,13 @@
+require 'date'
+require_relative '../ruby/item'
+
 class Movie
+  attr_accessor :silent, :publish_date
+
+  def initialize(silent, publish_date)
+    super(publish_date, archived)
+    @silent = silent
+    @publish_date = publish_date
+    @archived = archived
+  end
 end
