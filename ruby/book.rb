686d3cc0e123dd782f3ad97ecb46853cdf679cc9
OkoyeCharles <okoyecharles509@gmail.com>
2022-10-10 17:21:40 +0100

    Define Book Class Properties

--- ruby/book.rb
+++ ruby/book.rb
@@ -1,5 +1,11 @@
-class Book
-  def initialize
-    p 'To be implemented...'
+require_relative 'item'
+
+class Book < Item
+  attr_accessor :cover_state, :publisher
+
+  def initialize(publisher, cover_state, publish_date, archived)
+    super(publish_date, archived)
+    @publisher = publisher
+    @cover_state = cover_state
   end
 end
