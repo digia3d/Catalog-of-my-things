17decc5f3c08d6f053408b3372ed616c8cbe81bb
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-10-10 14:33:12 +0100

    Add Item Class Methods

--- ruby/item.rb
+++ ruby/item.rb
@@ -1,10 +1,18 @@
 class Item
-  attr_accessor :publish_date, :archived, :genre, :author, :source, :label
-  attr_reader :id
+  attr_accessor :publish_date, :archived
+  attr_writer :genre, :author, :source, :label
 
   def initialize(publish_date, archived)
     @id = Random.rand(1...1000)
     @publish_date = publish_date
     @archived = archived
   end
+
+  def move_to_archive; end
+
+  private
+
+  attr_reader :id
+
+  def can_be_archived?; end
 end
