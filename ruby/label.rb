44359f1e0e349641a94ae6fc9d98c9a0684d3f70
OkoyeCharles <okoyecharles509@gmail.com>
2022-10-10 17:25:56 +0100

    Define Label Class Properties

--- ruby/label.rb
+++ ruby/label.rb
@@ -1,5 +1,14 @@
 class Label
-  def initialize
-    p 'To be implemented...'
+  attr_accessor :title, :color
+
+  def initialize(title, color)
+    @id = Random.rand(1...1000)
+    @title = title
+    @color = color
+    @items = []
   end
+
+  private
+
+  attr_reader :id, :items
 end
