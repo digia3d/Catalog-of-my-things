bdf792076fc92dc67986fc62b447355c0f7897af
OkoyeCharles <okoyecharles509@gmail.com>
2022-10-11 09:46:38 +0100

    Fix errors

--- ruby/author.rb
+++ ruby/author.rb
@@ -1,10 +1,7 @@
-require_relative './item'
-
-class Author < Item
-  attr_accessor :id, :first_name, :last_name, :items
+class Author
+  attr_accessor :first_name, :last_name
 
   def initialize(first_name, last_name)
-    super
     @id = Random.rand(1..1000)
     @first_name = first_name
     @last_name = last_name
@@ -12,7 +9,11 @@ class Author < Item
   end
 
   def add_item(item)
-    items.push(item)
+    @items << item
     item.author = self
   end
+
+  private
+
+  attr_reader :id, :items
 end
--- ruby/game.rb
+++ ruby/game.rb
@@ -3,15 +3,15 @@ require_relative './item'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at, :publish_date
 
-  def initialize(multiplayer, last_played_at, publish_date)
-    super publish_date
+  def initialize(multiplayer, last_played_at, publish_date, archived)
+    super(publish_date, archived)
     @multiplayer = multiplayer
-    @last_played_at = last_played_at
+    @last_played_at = Date.iso8601(last_played_at)
   end
 
+  private
+
   def can_be_archived?
-    Date.today >
-      Date.iso8601(@last_played_at).next_year(2) &&
-      super
+    (Date.today > @last_played_at.next_year(2)) && super
   end
 end
