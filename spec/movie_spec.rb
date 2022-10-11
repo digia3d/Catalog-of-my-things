47accc4e70f28eb20f5d644c93391b0bd28f1d5a
OkoyeCharles <okoyecharles509@gmail.com>
2022-10-11 16:25:43 +0100

    Fix errors

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -3,25 +3,25 @@ require_relative '../ruby/movie'
 require 'date'
 
 describe Movie do
-    before(:each) do
-      @movie = Movie.new(true, '2020-01-10', false)
+  before(:each) do
+    @movie = Movie.new(true, '2020-01-10', false)
+  end
+
+  context '#new' do
+    it 'should initialize' do
+      expect(@movie).to be_an_instance_of(Movie)
     end
 
-    context '#new' do
-      it 'should initialize' do
-        expect(@movie).to be_an_instance_of(Movie)
-      end
+    it 'should assign right properties' do
+      expect(@movie).to have_attributes(
+        silent: true,
+        publish_date: Date.iso8601('2020-01-10'),
+        archived: false
+      )
+    end
 
-      it 'should assign right properties' do
-        expect(@movie).to have_attributes(
-          silent: true,
-          publish_date: Date.iso8601('2020-01-10'),
-          archived: false
-        )
-      end
-
-      it 'should be a sub-class of Item' do
-        expect(@movie.class).to be < Item
-      end
+    it 'should be a sub-class of Item' do
+      expect(@movie.class).to be < Item
     end
   end
+end
