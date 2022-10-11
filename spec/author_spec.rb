aef0fe255f8438ccef10d047cf15d24e7472e816
Aleksandra Ujvari <ujvari65@gmail.com>
2022-10-11 14:50:01 +0200

    Fix tests

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -8,15 +8,15 @@ describe Author do
 
   context 'should create a new author' do
     it 'takes two parameters for the author' do
-      @author.should be_an_instance_of Author
+      expect(@author).to be_an_instance_of Author
     end
 
     it 'should return the author first_name' do
-      @author.first_name.should == 'Aleksandra'
+      expect(@author.first_name).to eq 'Aleksandra'
     end
 
     it 'should return the author last_name' do
-      @author.last_name.should == 'Ujvari'
+      expect(@author.last_name).to eq 'Ujvari'
     end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,17 +2,15 @@ require_relative '../ruby/game'
 
 describe Game do
   before :each do
-    @game = [Game.new(true, '2020-10-11', '2022-10-13', false)]
+    @game = Game.new(true, '2020-10-11', '2022-10-13', false)
   end
 
   it 'should be the instance of class' do
-    @game.each do |game|
-      expect(game).to be_instance_of Game
-    end
+    expect(@game).to be_instance_of Game
   end
 
   it 'check if the game can be archived' do
-    can_be_archived = @game[0].send(:can_be_archived?)
+    can_be_archived = @game.send(:can_be_archived?)
     expect(can_be_archived).to be false
   end
 end
