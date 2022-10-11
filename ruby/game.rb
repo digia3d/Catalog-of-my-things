af63debc7293a645fac3d52e750d254ba66e9497
Aleksandra Ujvari <ujvari65@gmail.com>
2022-10-11 09:14:34 +0200

    Fix linters

--- ruby/game.rb
+++ ruby/game.rb
@@ -10,6 +10,8 @@ class Game < Item
   end
 
   def can_be_archived?
-    Date.today > Date.iso8601(@last_played_at).next_year(2) && super
+    Date.today >
+      Date.iso8601(@last_played_at).next_year(2) &&
+      super
   end
 end
