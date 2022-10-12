c6475dfbc8d649cf6b250e24731e28fa33eb4cb8
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-10-12 15:00:47 +0100

    Pass archived arguement as false

--- ruby/movie.rb
+++ ruby/movie.rb
@@ -3,8 +3,8 @@ require_relative 'item'
 class Movie < Item
   attr_accessor :silent
 
-  def initialize(silent, publish_date, archived)
-    super(publish_date, archived)
+  def initialize(silent, publish_date)
+    super(publish_date, false)
     @silent = silent
   end
 
