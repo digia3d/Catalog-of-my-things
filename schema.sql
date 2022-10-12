d4c8e0011d0c5abbe11278fd62fdf5420af80e2d
OkoyeCharles <okoyecharles509@gmail.com>
2022-10-12 15:51:30 +0100

    Create Book table

--- schema.sql
+++ schema.sql
@@ -1,9 +1,18 @@
-CREATE TABLE Item (
+CREATE TABLE item (
   id INT GENERATED ALWAYS AS DEFAULT,
   genre_id INT REFERENCES genre (id),
   author_id INT REFERENCES author (id),
   source_id INT REFERENCES source (id),
   label_id INT REFERENCES label (id),
+  publish_date DATE,
+  archived BOOLEAN,
+  PRIMARY KEY id,
+)
+
+CREATE TABlE book (
+  id INT GENERATED ALWAYS AS DEFAULT,
+  publisher VARCHAR(200),
+  cover_state VARCHAR(20),
   publish_date DATE,
   archived BOOLEAN,
   PRIMARY KEY id,
